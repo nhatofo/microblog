d567b2aeff0a175ff27a964ff483adb3240edce2
Teofilo Chichume <ChichumeT@mozltc.com>
2020-04-29 13:09:18 +0200

    first commit

---
+++ app/routes.py
@@ -0,0 +1,39 @@
+from flask import render_template, flash, redirect
+from app import app
+from app.forms import LoginForm
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+@app.route('/')
+@app.route('/index')
+
+class Config(object):
+    # ...
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
